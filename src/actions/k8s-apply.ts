abc23aff34f70f39b7c6bf62ef645fe085320069
Manabu McCloskey <manabu.mccloskey@gmail.com>
2023-07-25 15:36:44 -0700

    allow for cluster selection and more input types in k8s apply (#2)
    * allow for cluster selection and more input types in k8s apply
    
    * fix export name
    
    Signed-off-by: Jesse Sanford <jesse.sanford@autodesk.com>
    
    * added missing dependencies
    
    Signed-off-by: Jesse Sanford <jesse.sanford@autodesk.com>
    
    ---------
    
    Signed-off-by: Jesse Sanford <jesse.sanford@autodesk.com>
    Co-authored-by: Jesse Sanford <jesse.sanford@autodesk.com>

--- src/actions/k8s-apply.ts
+++ src/actions/k8s-apply.ts
@@ -1,46 +1,100 @@
-import {createTemplateAction} from '@backstage/plugin-scaffolder-backend';
-import {KubeConfig, CustomObjectsApi} from '@kubernetes/client-node';
-import yaml from 'js-yaml';
+import { createTemplateAction } from '@backstage/plugin-scaffolder-node';
+import { KubeConfig, CustomObjectsApi } from '@kubernetes/client-node';
+import YAML from 'yaml';
+import { Config } from '@backstage/config';
+import { resolveSafeChildPath } from '@backstage/backend-common';
+import fs from 'fs-extra';
 
-export const createKubernetesApply = () => {
+export const createKubernetesApply = (config: Config) => {
     return createTemplateAction<{
-        manifest: string;
+        manifestString?: string;
+        manifestObject?: any;
+        manifestPath?: string;
         namespaced: boolean;
+        clusterName?: string;
     }>({
         id: 'cnoe:kubernetes:apply',
         schema: {
             input: {
                 type: 'object',
-                required: ['manifest', 'namespaced'],
+                required: ['namespaced'],
                 properties: {
-                    manifest: {
+                    manifestString: {
                         type: 'string',
                         title: 'Manifest',
-                        description: 'The manifest to apply in the cluster',
+                        description: 'The manifest to apply in the cluster. Must be a string',
+                    },
+                    manifestObject: {
+                        type: 'object',
+                        title: 'Manifest',
+                        description: 'The manifest to apply in the cluster. Must be an object',
+                    },
+                    manifestPath: {
+                        type: 'string',
+                        title: 'Path to the manifest file',
+                        description: 'The path to the manifest file.',
                     },
                     namespaced: {
                         type: 'boolean',
                         title: 'Namespaced',
                         description: 'Whether the API is namespaced or not',
                     },
+                    clusterName: {
+                        type: 'string',
+                        title: 'Cluster Name',
+                        description: 'The name of the cluster to apply this',
+                    },
                 },
             },
         },
         async handler(ctx) {
-            const obj: any = yaml.load(ctx.input.manifest);
+            let obj: any;
+            if (ctx.input.manifestString) {
+                obj = YAML.parse(ctx.input.manifestString);
+            } else if (ctx.input.manifestObject) {
+                obj = ctx.input.manifestObject;
+            } else {
+                const filePath = resolveSafeChildPath(
+                  ctx.workspacePath,
+                  ctx.input.manifestPath!,
+                );
+                const fileContent = fs.readFileSync(filePath, 'utf8');
+                obj = YAML.parse(fileContent);
+            }
             const words = obj.apiVersion.split('/');
             const group = words[0];
             const version = words[1];
             //hack. needs fixing to correctly extract the plurals
-            const plural = obj.kind.toLowerCase() + 's';
-
-            // hack. needs fixing to get the KubeConfig info from app-config.yaml
+            const plural = `${obj.kind.toLowerCase()}s`;
             const kc = new KubeConfig();
-            kc.loadFromDefault();
+            if (ctx.input.clusterName) {
+                // Supports SA token authentication only
+                const targetCluster = getClusterConfig(ctx.input.clusterName!, config);
+                kc.addCluster({
+                    name: targetCluster.getString('name'),
+                    caData: targetCluster.getString('caData'),
+                    server: targetCluster.getString('url'),
+                    skipTLSVerify: targetCluster.getBoolean('skipTLSVerify'),
+                });
+                kc.addUser({
+                    name: 'scaffolder-user',
+                    token: targetCluster.getString('serviceAccountToken'),
+                });
+                kc.addContext({
+                    cluster: ctx.input.clusterName,
+                    user: 'scaffolder-user',
+                    name: ctx.input.clusterName,
+                });
+                kc.setCurrentContext(ctx.input.clusterName);
+            } else {
+                kc.loadFromDefault()
+            }
+
             const client = kc.makeApiClient(CustomObjectsApi);
             // Server-side apply.
             if (ctx.input.namespaced) {
-                await client.patchNamespacedCustomObject(
+                await client
+                  .patchNamespacedCustomObject(
                     group,
                     version,
                     obj.metadata.namespace,
@@ -50,23 +104,31 @@ export const createKubernetesApply = () => {
                     undefined,
                     'backstage',
                     true,
-                    {headers: {'Content-Type': 'application/apply-patch+yaml'}}
-                ).then(
-                    (resp) => {
+                    { headers: { 'Content-Type': 'application/apply-patch+yaml' } },
+                  )
+                  .then(
+                    resp => {
                         ctx.logger.info(
-                            `Successfully created ${obj.metadata.namespace}/${obj.metadata.name} Application: HTTP ${resp.response.statusCode}`
+                          `Successfully created ${obj.metadata.namespace}/${obj.metadata.name} Application: HTTP ${resp.response.statusCode}`,
                         );
                     },
-                    (err) => {
+                    err => {
                         ctx.logger.error(
-                            `Failed to make PATCH call for ${obj.metadata.namespace}/${obj.metadata.name} Application: Body ${JSON.stringify(err.body, null, 2)} Response ${JSON.stringify(err.response, null, 2)}.`
+                          `Failed to make PATCH call for ${obj.metadata.namespace}/${
+                            obj.metadata.name
+                          } Application: Body ${JSON.stringify(
+                            err.body,
+                            null,
+                            2,
+                          )} Response ${JSON.stringify(err.response, null, 2)}.`,
                         );
                         throw err;
-                    }
-                )
+                    },
+                  );
                 return;
             }
-            await client.patchClusterCustomObject(
+            await client
+              .patchClusterCustomObject(
                 group,
                 version,
                 plural,
@@ -75,20 +137,51 @@ export const createKubernetesApply = () => {
                 undefined,
                 'backstage',
                 true,
-                {headers: {'Content-Type': 'application/apply-patch+yaml'}}
-            ).then(
-                (resp) => {
+                { headers: { 'Content-Type': 'application/apply-patch+yaml' } },
+              )
+              .then(
+                resp => {
                     ctx.logger.info(
-                        `Successfully created ${obj.metadata.name} Application: HTTP ${resp.response.statusCode}`
+                      `Successfully created ${obj.metadata.name} Application: HTTP ${resp.response.statusCode}`,
                     );
                 },
-                (err) => {
+                err => {
                     ctx.logger.error(
-                        `Failed to make PATCH call for ${obj.metadata.name} Application: Body ${JSON.stringify(err.body, null, 2)} Response ${JSON.stringify(err.response, null, 2)}.`
+                      `Failed to make PATCH call for ${
+                        obj.metadata.name
+                      } Application: Body ${JSON.stringify(
+                        err.body,
+                        null,
+                        2,
+                      )} Response ${JSON.stringify(err.response, null, 2)}.`,
                     );
                     throw err;
-                }
-            )
+                },
+              );
         },
     });
 };
+
+// Finds the first cluster that matches the given name.
+function getClusterConfig(name: string, config: Config): Config {
+    const clusterConfigs = config
+      .getConfigArray('kubernetes.clusterLocatorMethods')
+      .filter((val: Config) => {
+          return val.getString('type') === 'config';
+      });
+
+    const clusters = new Array<Config>();
+    clusterConfigs.filter((conf: Config) => {
+        const cluster = conf.getConfigArray('clusters').find((val: Config) => {
+            return val.getString('name') === name;
+        });
+        if (cluster) {
+            clusters.push(cluster);
+        }
+    });
+
+    if (clusters.length === 0) {
+        throw new Error(`Cluster with name ${name} not found`);
+    }
+    return clusters[0];
+}
